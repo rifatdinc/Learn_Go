626f793386017de949cf4d5d16d2a7eb544e400f
Rifat Dinc <rafidinc41@gmail.com>
2022-10-04 15:17:24 +0300

    Added To Chan

--- main.go
+++ main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"Workspace/Datatype"
-	s "Workspace/Route_s"
+	"fmt"
 )
+
+type Longtimes struct {
+	Ints int
+	Sd   struct {
+		ints int
+	}
+}
 
 func main() {
 	// s.Loops("10.50.1.9", "")
-	s.Nmap("192.168.14.1", "8728")
-	Datatype.Pointer("2")
-	Datatype.Structs()
-	Datatype.StructsField()
-	Datatype.PointerStruct()
-	Datatype.StructInfo()
-	Datatype.Arrays()
-	Datatype.Interfaces()
-	Datatype.Ipadress()
+	// s.Nmap("192.168.14.1", "8728")
+	// Datatype.Pointer("2")
+	// Datatype.Structs()
+	// Datatype.StructsField()
+	// Datatype.PointerStruct()
+	// Datatype.StructInfo()
+	// Datatype.Arrays()
+	// Datatype.Interfaces()
+	// Datatype.Ipadress()
+	Datatype.Longtime()
+	Longtimes.example(Longtimes{
+		Ints: 7,
+		Sd:   struct{ ints int }{
+			ints: 3,
+		},
+	})
+}
+
+func (c Longtimes) example() {
+	fmt.Println(c.Sd.ints)
 }
